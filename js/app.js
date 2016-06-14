d85165f546ba002cc4cd42311b12cd3a737fc55f
banyudu <banyudu@gmail.com>
2016-06-14 13:27:58 +0800

    add electron files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 // app.js
 
 function doSearch(word) {
-	alert(word);
+	'use strict';
+	let website = "https://zh.wikipedia.org";
+	let wikiUrl = "/wiki/";
 
+	// set content of search bar
+	$("#content").val(word);
 	// return false to stop submit
+
+	// mediawiki api cannot meet the requirements, so download html page directly instead of use api.
+
+	// download page source
+	jQuery.ajax({  
+        url: website + wikiUrl + encodeURIComponent(word),  
+        type: "GET",  
+        success: function (data) {  
+			console.log(data);
+        },  
+        dataType: "html"  
+    }); 
 	return false;
 }
